073e56275befae344df124d79a8e4fe677c14ccd
TechWarrior <roshnijadhav118@gmail.com>
2026-08-25 21:31:11 +0530

    Implement isAnagram method in Solution class

---
+++ Anagram-242.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        count_s = {}
+        count_t = {}
+
+        for ch in s:
+            count_s[ch] = count_s.get(ch, 0) + 1
+
+        for ch in t:
+            count_t[ch] = count_t.get(ch, 0) + 1
+
+        return count_s == count_t
