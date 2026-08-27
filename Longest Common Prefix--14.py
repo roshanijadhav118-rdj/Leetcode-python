7ce712e786e910d412e5559b9820e1cd8e7f7e6a
TechWarrior <roshnijadhav118@gmail.com>
2026-08-27 21:05:54 +0530

    Implement longest common prefix function

---
+++ Longest Common Prefix--14.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        for i in range(len(strs[0])):
+            char = strs[0][i]
+
+            for word in strs:
+                if i == len(word) or word[i] != char:
+                    return strs[0][:i]
+
+        return strs[0]
