d31d321ebdae5d53082b14e3ddee92bffeea4331
TechWarrior <roshnijadhav118@gmail.com>
2026-08-28 19:41:35 +0530

    Add isIsomorphic function to check string isomorphism
    Implement isIsomorphic method to check if two strings are isomorphic.

---
+++ Isomorphic strings-205.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isIsomorphic(self, s, t):
+        s_to_t = {}
+        t_to_s = {}
+
+        for i in range(len(s)):
+            if s[i] in s_to_t:
+                if s_to_t[s[i]] != t[i]:
+                    return False
+
+            if t[i] in t_to_s:
+                if t_to_s[t[i]] != s[i]:
+                    return False
+
+            s_to_t[s[i]] = t[i]
+            t_to_s[t[i]] = s[i]
+
+        return True
