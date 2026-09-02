4c56a588b662b7d8560b6ee9068e6ee63f170c06
TechWarrior <roshnijadhav118@gmail.com>
2026-09-02 20:18:15 +0530

    Implement longest consecutive sequence function

---
+++ Longest consecutive sequence -128.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def longestConsecutive(self, nums):
+        num_set = set(nums)
+        longest = 0
+
+        for x in num_set:
+
+            if x - 1 not in num_set:
+
+                length = 1
+              
+                while x + length in num_set:
+                    length += 1
+
+                longest = max(longest, length)
+
+        return longest
